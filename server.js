f353dd5e98bad988865e2d583b056f27ab997f3a
Nanut Lertmahakiat <onlyseason@hotmail.com>
2015-12-02 11:48:06 +0700

    Add POST /todos Route

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: "Meet mom for lunch",
-	completed: false
-}, {
-	id: 2,
-	description: "Go to market",
-	completed: false
-}, {
-	id: 3,
-	description: "Feed the dog",
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
 	res.send('Todo API root');
@@ -46,6 +38,19 @@ app.get('/todos/:id', function (req, res) {
 	//res.json(todos, {id: req.params.id});
 });
 
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	// add id field
+	body.id = todoNextId++;
+
+	// push body into array
+	todos.push(body);
+
+	res.json(body);
+});
+
 app.listen(PORT , function () {
 	console.log("Server is running on port " + PORT);
-})
+});
