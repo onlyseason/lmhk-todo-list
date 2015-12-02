0061700089b799d6b65370a995fe7517082d8ebe
Nanut Lertmahakiat <onlyseason@hotmail.com>
2015-12-02 13:47:02 +0700

    Refactor Code with Underscore

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -19,28 +20,25 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
-
-	//res.status(404).send();
-
-	//res.json(todos, {id: req.params.id});
 });
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	//var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	// add id field
 	body.id = todoNextId++;
